4073722105c11174d1352bf4794865eed06b31d0
hyunsu <coding8282@gmail.com>
2018-07-04 06:15:17 +0900

    운영환경설정 이관

--- integration/prod/scripts/start-app.sh
+++ integration/prod/scripts/start-app.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-nohup java -jar \
-/home/ec2-user/okky-comment-1.0.0.jar \
-> /dev/null 2> /dev/null < /dev/null &
+sudo chmod +x /home/ec2-user/okky-comment-1.0.0.jar
+sudo ln -sf /home/ec2-user/okky-comment-1.0.0.jar /etc/init.d/okky-comment
+sudo service okky-comment start
+sleep 10s
